8cfffd4b491e3d00fde5c1868148ecd9ed48634d
luzenobi@gmail.com <luzenobi@gmail.com@baa6f028-fc34-b318-572c-e03319f6152e>
2015-01-24 17:34:14 +0000

    Various updates: - ShaderFactory is now a singleton - Changed draw method of SceneComplexEvoObj

--- res/raw/shader_vertex_complex_object.glsl
+++ res/raw/shader_vertex_complex_object.glsl
@@ -3,7 +3,7 @@ invariant gl_Position;
 
 const vec4 col = vec4(0.0, 0.5, 0.5, 1.0);
 
-uniform bool enableColorVector = false;
+uniform float enableColorVector = 0.0;
 uniform mat4 u_mvpMatrix;
 layout(location = 0) in vec4 a_position;
 layout(location = 1) in vec4 a_color;
@@ -11,7 +11,7 @@ out vec4 v_color;
 
 void main()
 {
-    if (enableColorVector) {
+    if (enableColorVector == 1.0) {
         v_color = a_color;
     } else {
         v_color = col;
