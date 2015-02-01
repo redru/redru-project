20c4af46b67f553b7c25a248aaf0192f2c9c2698
luzenobi@gmail.com <luzenobi@gmail.com@baa6f028-fc34-b318-572c-e03319f6152e>
2015-02-01 09:19:50 +0000

    Added textures to main starship

--- res/raw/shader_vertex_complex_object.glsl
+++ res/raw/shader_vertex_complex_object.glsl
@@ -5,13 +5,14 @@ const vec4 col = vec4(0.0, 0.5, 0.5, 1.0);
 
 uniform mat4 u_mvpMatrix;
 layout(location = 0) in vec4 a_position;
-layout(location = 1) in vec4 a_color;
-out vec4 v_color;
+layout(location = 1) in vec2 a_texCoord;
+out vec2 v_texCoord;
 
 void main()
 {
-    v_color = a_color;
+    //v_color = a_color;
     //v_color = col;
 
     gl_Position = u_mvpMatrix * a_position;
+    v_texCoord = a_texCoord;
 }
